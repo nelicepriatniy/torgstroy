5ac27cc5f2232a8dd5d0ffb1587204b5fd2bb4ab
nelicepriatniy <92029297+nelicepriatniy@users.noreply.github.com>
2023-09-05 17:19:31 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -13,19 +13,37 @@ if(menuBlock) {
 
 const partnersSlider = new Swiper('.partners-slider', {
     speed: 400,
-    slidesPerView: 4,
-    spaceBetween: 70,
     navigation: {
       nextEl: '.partners-slider__arrow-next',
       prevEl: '.partners-slider__arrow-prew',
     },
+    breakpoints: {
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 30,
+        },
+        // when window width is >= 480px
+        1024: {
+            slidesPerView: 4,
+            spaceBetween: 70,
+        },
+      }
 });
 const sertifSlider = new Swiper('.sertif-slider', {
     speed: 400,
-    slidesPerView: 3,
-    spaceBetween: 80,
     navigation: {
       nextEl: '.sertif-next-img',
       prevEl: '.sertif-prev-img',
     },
+    breakpoints: {
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 30,
+        },
+        // when window width is >= 480px
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 80,
+        },
+      }
 });
